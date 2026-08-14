fd12dd08a028cb4f39b2407aee9cfa7aa729da26
unknown <owusukofieric79@gmail.com>
2026-08-14 18:02:26 +0000

    changes

--- adminSeeder.js
+++ adminSeeder.js
@@ -13,24 +13,24 @@ const createAdmin = async () => {
 
     if (admin) {
       admin.name = 'Mirova Admin'
-      admin.email = 'owusukofieric79@gmail.com'
-      admin.password = 'Swift.eric79'
+      admin.email = 'mirova.jewelleryy@gmail.com'
+      admin.password = 'Swift.mirova79'
       admin.phone = '055 906 4813'
       await admin.save()
       console.log('✅ Admin credentials successfully updated!')
-      console.log('Email: owusukofieric79@gmail.com')
-      console.log('Password: Swift.eric79')
+      console.log('Email: mirova.jewelleryy@gmail.com')
+      console.log('Password: Swift.mirova79')
     } else {
       await User.create({
         name: 'Mirova Admin',
-        email: 'owusukofieric79@gmail.com',
-        password: 'Swift.eric79',
+        email: 'mirova.jewelleryy@gmail.com',
+        password: 'Swift.mirova79',
         phone: '055 906 4813',
         role: 'admin',
       })
       console.log('✅ Admin account successfully created!')
-      console.log('Email: owusukofieric79@gmail.com')
-      console.log('Password: Swift.eric79')
+      console.log('Email: mirova.jewelleryy@gmail.com')
+      console.log('Password: Swift.mirova79')
     }
 
     process.exit()
